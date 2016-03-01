3c1627719a0dee8a992460afd063a77fddc1ab8f
Thalmann <mr.thalmann@gmail.com>
2016-03-01 14:01:33 +0100

    Added operators to the label visitor

--- generate_cfg.py
+++ generate_cfg.py
@@ -40,14 +40,14 @@ class Listener(ast.NodeVisitor):
         CFG.append((label.result, vars.result))
 
     def visit_Call(self, node):
-        v = Vars()        
-        if node.func.id == 'input':
-            v.visit(node)
-            CFG.append((v.result))
-        elif node.func.id == 'print':
-            v.visit(node)
-            CFG.append((node.func.id, node.args, node.keywords))
+        v = Vars()
+        label_visitor = LabelVisitor()
+        
+        v.visit(node)
+        label_visitor.visit(node)
+        CFG.append((label_visitor.result, v.result))
 
+    
     def visit_Num(self, node):
         return node.n
 
@@ -57,7 +57,13 @@ class Vars(ast.NodeVisitor):
         self.result.append(node.id)
 
     def visit_Call(self,node):
-        self.result.append((node.args,node.keywords))
+        for arg in node.args:
+            self.visit(arg)
+        for keyword in node.keywords:
+            self.visit(keyword)
+
+    def visit_keyword(self, node):
+        self.visit(node.value)
     
     def __init__(self):
         self.result = list()
@@ -74,6 +80,84 @@ class LabelVisitor(ast.NodeVisitor):
         for op,com in zip(node.ops,node.comparators):
             self.visit(op)
             self.visit(com)
+
+    def visit_BinOp(self, node):
+        self.visit(node.left)
+        self.visit(node.op)
+        self.visit(node.right)
+
+    #  operator = Add | Sub | Mult | MatMult | Div | Mod | Pow | LShift | RShift | BitOr | BitXor | BitAnd | FloorDiv
+    def visit_Add(self, node):
+        self.result = ' '.join((self.result, '+'))
+
+    def visit_Sub(self, node):
+        self.result = ' '.join((self.result, '-'))
+
+    def visit_Mult(self, node):
+        self.result = ' '.join((self.result, '*'))
+
+    def vist_MatMult(self, node):
+        self.result = ' '.join((self.result, 'x'))
+
+    def visit_Div(self, node):
+        self.result = ' '.join((self.result, '/'))
+
+    def visit_Mod(self, node):
+        self.result = ' '.join((self.result, '%'))
+
+    def visit_Pow(self, node):
+        self.result = ' '.join((self.result, '**'))
+
+    def visit_LShift(self, node):
+        self.result = ' '.join((self.result, '<<'))
+
+    def visit_RShift(self, node):
+        self.result = ' '.join((self.result, '>>'))
+
+    def visit_BitOr(self, node):
+        self.result = ' '.join((self.result, '|'))
+
+    def visit_BitXor(self, node):
+        self.result = ' '.join((self.result, '^'))
+
+    def visit_BitAnd(self, node):
+        self.result = ' '.join((self.result, '&'))
+
+    def visit_FloorDiv(self, node):
+        self.result = ' '.join((self.result, '//'))
+
+        
+    # cmpop = Eq | NotEq | Lt | LtE | Gt | GtE | Is | IsNot | In | NotIn    
+    def visit_Eq(self, node):
+        self.result = ' '.join((self.result, '=='))
+        
+    def visit_Gt(self, node):
+        self.result = ' '.join((self.result,'>'))
+        
+    def visit_Lt(self, node):
+        self.result = ' '.join((self.result,'<'))
+        
+    def visit_NotEq(self,node):
+        self.result = ' '.join((self.result,'!='))
+        
+    def visit_GtE(self,node):
+        self.result = ' '.join((self.result,'>='))
+        
+    def visit_LtE(self,node):
+        self.result = ' '.join((self.result,'<='))
+        
+    def visit_Is(self,node):
+        self.result = ' '.join((self.result,'is'))
+
+    def visit_IsNot(self,node):
+        self.result = ' '.join((self.result,'is not'))
+                 
+    def visit_In(self,node):
+        self.result = ' '.join((self.result,'in'))
+
+    def visit_NotIn(self,node):
+        self.result = ' '.join((self.result,'not in'))
+                 
     def visit_Num(self, node):
         self.result = ' '.join((self.result, str(node.n)))
     def visit_Name(self,node):
@@ -89,3 +173,7 @@ Listener().visit(obj)
 for n in CFG:
     print(n)
         
+
+
+
+
